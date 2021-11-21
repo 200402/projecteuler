1cdfe423b1fae3570f79910f5b2f58adcfa5f149
200402 <r_he_lol@mail.ru>
2021-11-21 21:56:13 +0500

    Добавьте файлы проекта.

---
+++ projecteuler/1-100/1-10/task_1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projecteuler
+{
+    public class task_1
+    {
+        int[] arrayOfMultiplicity = new int[] { 3, 5 };
+        int start = 0;
+        int end = 100000;
+
+        public long answer()
+        {
+            long sum = 0;
+            bool fit;
+
+            for (int i = start; i < end; i++)
+            {
+                fit = false;
+                for (int ii = 0; ii < arrayOfMultiplicity.Length; ii++)
+                {
+                    if (i % arrayOfMultiplicity[ii] == 0)
+                        fit = true;
+                }
+                if (fit)
+                    sum += i;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ projecteuler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace projecteuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                start(Console.ReadLine());
+            }
+        }
+
+        static void start(string taskNumber)
+        {
+            switch (taskNumber)
+            {
+                case "1":
+                    var Task_1 = new task_1();
+                    Console.WriteLine(Task_1.answer());
+                    break;
+            }
+        }
+
+    }
+}
